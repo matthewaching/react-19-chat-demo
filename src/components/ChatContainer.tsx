0336195fd4d5071976f39b598ccb07c07c1936b2
unknown <matthewaching@gmail.com>
2024-08-19 09:16:26 -0500

    Add more wireframing

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,6 +1,18 @@
 const ChatContainer = () => {
     return (
-        <div>ChatContainer</div>
+        <div className='chatContainer'>
+            <div className='chatHeader'>
+                <div className='chatAvatar'>A</div>
+                <span>Header</span>
+            </div>
+            <div className='chatMessages'>Messages</div>
+            <div className='chatInput'>
+                <form>
+                    <input></input>
+                    <button></button>
+                </form>
+            </div>
+        </div>
     );
 };
 
